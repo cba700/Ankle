4db96386b598d8b5481e359d7a4faaa1e336648b
limchangin <dlackddls5@naver.com>
2026-04-10 01:54:46 +0900

    ㅇ

--- src/components/home/home-page-skeleton.tsx
+++ src/components/home/home-page-skeleton.tsx
@@ -3,27 +3,7 @@ import styles from "./home-page-skeleton.module.css";
 export function HomePageSkeleton() {
   return (
     <div className={styles.page}>
-      <div className={styles.shell}>
-        <div className={styles.header}>
-          <div className={styles.brand}>앵클</div>
-          <div className={styles.headerTabs}>
-            <div className={styles.tab} />
-            <div className={styles.tab} />
-          </div>
-        </div>
-        <div className={styles.hero} />
-        <div className={styles.filters}>
-          <div className={styles.filterChip} />
-          <div className={styles.filterChip} />
-          <div className={styles.filterChip} />
-        </div>
-        <div className={styles.heading} />
-        <div className={styles.cards}>
-          <div className={styles.card} />
-          <div className={styles.card} />
-          <div className={styles.card} />
-        </div>
-      </div>
+      <div className={styles.wordmark}>앵클</div>
     </div>
   );
 }
